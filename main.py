30b3d07577c9ee5a842fe9ed5a35471b3daeeb01
jrodr117 <jrodr113@msudenver.edu>
2022-10-06 16:58:11 -0600

    removed test print statement

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ def sales_by_quarter(sales):
     for row in range(len(sales)):
         for i in range(len(sales[row])):
             quarter_sales_array[i] += sales[row][i]
-        print(quarter_sales_array)
     for row in range(len(quarter_sales_array)):
         print(f"Q{(quarter_counter)}: {(quarter_sales_array[row])}")
         quarter_counter += 1
