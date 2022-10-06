4d915c268be7526e552c15764cdfec68f36d496e
jrodr117 <jrodr113@msudenver.edu>
2022-10-06 17:30:24 -0600

    imported locale module to us currency function in order to display proper currency formatting

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import locale as lc
+from re import T
+
+lc.setlocale(lc.LC_ALL, "en_us")
 
 
 def sales_report(sales):
     region_counter = 1
     for row in sales:
-        print(f"{str(region_counter):9s}", end="")
+        print(f"{str(region_counter):12s}", end="")
         for item in row:
-            print(f"{item:10,.2f} ", end="")
+            # print(f"{item:10,.2f} ", end="")
+            print(f"{lc.currency(item, grouping=True)} ", end="")
         print()
         region_counter += 1
 
@@ -20,7 +24,9 @@ def sales_by_region(sales):
         for index in row:
             sum_of_index += index
         total += sum_of_index
-        print(f"Region {str(region_counter)}: {(sum_of_index):4,.2f}")
+        print(
+            f"Region {str(region_counter)}: {lc.currency(sum_of_index, grouping=True)}"
+        )
         region_counter += 1
     return total
 
@@ -34,7 +40,9 @@ def sales_by_quarter(sales):
         for i in range(len(sales[row])):
             quarter_sales_array[i] += sales[row][i]
     for row in range(len(quarter_sales_array)):
-        print(f"Q{(quarter_counter)}: {(quarter_sales_array[row]):4,.2f}")
+        print(
+            f"Q{(quarter_counter)}: {lc.currency(quarter_sales_array[row], grouping=True)}"
+        )
         quarter_counter += 1
 
 
@@ -46,11 +54,13 @@ def main():
         [1105.0, 4102.0, 2391.0, 1576.0],
     ]
     print("Sales Report\n")
-    print(f"{'Region':16s} {'Q1':10s} {'Q2':10s} {'Q3':10s} {'Q4':10s}")
+    print(f"{'Region':18s} {'Q1':9s} {'Q2':9s} {'Q3':9s} {'Q4'}")
     sales_report(sales)
     grand_total = sales_by_region(sales)
     sales_by_quarter(sales)
-    print(f"\nTotal annual sales, all regions: {grand_total:,.2f}")
+    print(
+        f"\nTotal annual sales, all regions: {lc.currency(grand_total, grouping=True)}"
+    )
 
 
 main()
